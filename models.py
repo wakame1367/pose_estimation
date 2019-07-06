58927289302bf17fed419e4745b0269d12b4330e
wakamezake <hotwater1367@gmail.com>
2019-07-06 12:34:16 +0900

    kp_maps

--- models.py
+++ models.py
@@ -1,4 +1,8 @@
+from utils import keypoints2index
+from tensorflow.python.keras.layers import Conv2D
 from tensorflow.python.keras.applications.mobilenet import MobileNet
+
+kp2index = keypoints2index()
 
 
 def posenet(base_model_name="mobilenet"):
@@ -8,3 +12,10 @@ def posenet(base_model_name="mobilenet"):
 
     if base_model_name == "mobilenet":
         base_model = MobileNet(include_top=False)
+    else:
+        raise ValueError()
+
+    kp_maps = Conv2D(len(kp2index),
+                     kernel_size=(1, 1),
+                     activation="sigmoid")(base_model.output)
+    return [kp_maps]
