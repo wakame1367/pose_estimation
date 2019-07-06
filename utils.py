71f4262a9d36c9b7a01186607f74c0d0296a9f2f
wakamezake <hotwater1367@gmail.com>
2019-07-06 13:28:54 +0900

    body_parts right/left indexes

--- utils.py
+++ utils.py
@@ -10,3 +10,19 @@ def keypoints2index():
         configs = yaml.load(f, Loader=yaml.FullLoader)
     keypoints = configs["keypoints"]
     return dict(zip(range(len(keypoints)), keypoints))
+
+
+def right_and_left_parts_indexes(ky2index):
+    """
+
+    :param ky2index: dict
+    :return: list, list
+    """
+    right_indexes = []
+    left_indexes = []
+    for part_idx, part_name in ky2index.items():
+        if part_name.startswith("R"):
+            right_indexes.append(part_idx)
+        elif part_name.startswith("L"):
+            left_indexes.append(part_idx)
+    return right_indexes, left_indexes
