7e844d9f27d0b576998258b4700530ae4bad2052
kot.yuhara <kot.yuhara@trifort.jp>
2019-07-12 12:27:28 +0900

    coco

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 
 def get_keypoints(keypoint_path):
@@ -18,6 +19,18 @@ def get_keypoints(keypoint_path):
     for annotation in keypoints["annotations"]:
         image_id = annotation["image_id"]
         if image_info.get(image_id):
-            image_info[image_id].append(annotation["keypoints"])
+            print(len(annotation["keypoints"]))
+            image_info[image_id].append(np.array(annotation["keypoints"]))
+        else:
+            image_info[image_id].append(None)
+    """
+    http://cocodataset.org/#format-data
+    x, y, v
+    x and y: location
+    v: visibility flag
+       - v=0: not labeled (in which case x=y=0)
+       - v=1: labeled but not visible
+       - v=2: labeled and visible
+    """
 
     return image_info
