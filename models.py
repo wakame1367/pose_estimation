b3610a2eaa1ee00542b64495d09479d91585e8a7
kot.yuhara <kot.yuhara@trifort.jp>
2019-07-18 18:08:32 +0900

    入力は224サイズの画像です。出力は2Dと3Dの両方を出力するようにしています。2Dは14x14x関節数のHeatMapと14x14x関節数x2（x,y座標）のオフセット値の出力です。

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
-from utils import keypoints2index
-from tensorflow.python.keras.layers import Conv2D
+from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.applications.mobilenet import MobileNet
+from tensorflow.python.keras.layers import Conv2D
+
+from utils import keypoints2index
 
 kp2index = keypoints2index()
 
@@ -11,11 +13,15 @@ def posenet(base_model_name="mobilenet"):
         raise ValueError("{} only.".format(base_model_names[0]))
 
     if base_model_name == "mobilenet":
-        base_model = MobileNet(include_top=False)
+        base_model = MobileNet(input_shape=(224, 224, 3), include_top=False)
+        # 14 * 14 * keypoints
+        target_layer = "conv_pw_11_relu"
+        new_model = Model(inputs=base_model.input,
+                          outputs=base_model.get_layer(name=target_layer).output)
     else:
         raise ValueError()
 
     kp_maps = Conv2D(len(kp2index),
                      kernel_size=(1, 1),
-                     activation="sigmoid")(base_model.output)
+                     activation="sigmoid")(new_model.output)
     return [kp_maps]
