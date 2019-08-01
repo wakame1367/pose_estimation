9ce7ae407a4d29a8b38f653cd307491a56b6ff92
kot.yuhara <kot.yuhara@trifort.jp>
2019-08-01 19:07:12 +0900

    add euclidean_distance_loss

--- losses.py
+++ losses.py
@@ -5,5 +5,9 @@ def offset_loss(y_true, y_pred):
     return kb.mean(kb.abs((y_pred - y_true)), axis=-1)
 
 
+def euclidean_distance_loss(y_true, y_pred):
+    return kb.sqrt(kb.sum(kb.square((y_pred - y_true)), axis=-1))
+
+
 def heatmap_loss(y_true, y_pred, coeff=4):
     return kb.mean(kb.binary_crossentropy(y_true, y_pred), axis=-1, keepdims=True) * coeff
